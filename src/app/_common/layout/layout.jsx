ea1816311dff28932cf1cf4f9f9a81cff5336c1a
neosharma995 <mytravplan@gmail.com>
2024-09-27 18:25:51 +0530

    updated code

--- src/app/_common/layout/layout.jsx
+++ src/app/_common/layout/layout.jsx
@@ -1,32 +1,44 @@
-'use client'
-import React, { useEffect, useState } from 'react'
-import Header from '../header/header'
-import Footer from '../footer/footer'
-import { EXPORT_ALL_APIS } from '@/utils/api/apis'
-import Navbar from '../header/navbar'
+'use client';
+import React, { useEffect, useState } from 'react';
+import Header from '../header/header';
+import Footer from '../footer/footer';
+import { EXPORT_ALL_APIS } from '@/utils/api/apis';
+import Navbar from '../header/navbar';
+import GlobalLoader from '../loaders/globalLoader';
 
+function Layout({ children }) {
+    let api = EXPORT_ALL_APIS();
+    let [result, setResult] = useState([]);
+    let [loading, setLoading] = useState(true);
 
-function Layout({children}) {
-  let api=EXPORT_ALL_APIS()
-  let [result,setResult]=useState([])
-  useEffect(()=>{
-    let loadHeaderAndFooter=async()=>{
-      let resp=await api.fetchHeaderFooterApi()
-      setResult(resp.data)
+    useEffect(() => {
+        let loadHeaderAndFooter = async () => {
+            setLoading(true);  
+            try {
+                let resp = await api.fetchHeaderFooterApi();
+                setResult(resp.data); 
+            } catch (error) {
+                console.error('bad request 404');
+            } finally {
+                setLoading(false);  
+            }
+        };
+
+        loadHeaderAndFooter();
+    }, []);
+
+    if (loading) {
+       return <GlobalLoader/>
     }
 
-    loadHeaderAndFooter()
-
-  },[])
-  return (
-    <>
-    <Header result={result}/>
-    <Navbar result={result}/>
-    {children}
-    <Footer result={result}/>
-
-    </>
-  )
+    return (
+        <>
+            <Header result={result} />
+            <Navbar result={result} />
+            {children}
+            <Footer result={result} />
+        </>
+    );
 }
 
-export default Layout
+export default Layout;
