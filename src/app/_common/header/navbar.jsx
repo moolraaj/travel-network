93da25c0e7a06ae5e53cae22d0704361fef34f69
Rakhi Sharma <rakhisharma98056@gmail.com>
2024-09-27 20:42:39 +0530

    header changes

--- src/app/_common/header/navbar.jsx
+++ src/app/_common/header/navbar.jsx
@@ -1,36 +1,44 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 
-function Navbar({result}) {
-    console.log(result)
-    let {header={}}=result
+function Navbar({ result }) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  let { header = {} } = result
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   return (
-     <>
-     <div className="navbar_outer">
+    <>
+      <div className="container">
         <div className="navbar inner">
-            <div className="navbar_wrapper">
-                <div className="navbar_section_first">
-                    <Link href={'/'}>
-                    <img src={header?.siteLogoUrl} alt="logo"  />
-                    </Link>
-                </div>
-                <div className="navbar_section_second">
-                    <ul className="navbar">
-                        <li><Link href={'/'}>home</Link></li>
-                        <li><Link href={'/about-us'}>about</Link></li>
-                        <li><Link href={'/tour-packages'}>tour packages</Link></li>
-                        <li><Link href={'/destinations'}>destinations</Link></li>
-                        <li><Link href={'/plan-your-trip'}>plan your trip</Link></li>
-                        <li><Link href={'/contact-us'}>contact us</Link></li>
-                    </ul>
-                </div>
-                <div className="navbar_section_third">
-                    <button>book now</button>
-                </div>
+          <div className="navbar_wrapper">
+            <div className="navbar_section_first">
+              <Link href={'/'}>
+                <img src={header?.siteLogoUrl} alt="logo" />
+              </Link>
             </div>
+            <div className={`navbar_section_second ${isMenuOpen ? 'active' : ''}`}>
+              <ul className="navbar">
+                <li><Link href={'/'}>home</Link></li>
+                <li><Link href={'/about-us'}>about</Link></li>
+                <li><Link href={'/tour-packages'}>tour packages</Link></li>
+                <li><Link href={'/destinations'}>destinations</Link></li>
+                <li><Link href={'/plan-your-trip'}>plan your trip</Link></li>
+                <li><Link href={'/contact-us'}>contact us</Link></li>
+              </ul>
+            </div>
+            <div className="navbar_section_third">
+              <button>book now</button>
+            </div>
+            <button className="toggle_button" onClick={toggleMenu}>
+              {isMenuOpen ? '✖' : '☰'}
+            </button>
+          </div>
         </div>
-     </div>
-     </>
+      </div>
+    </>
   )
 }
 
